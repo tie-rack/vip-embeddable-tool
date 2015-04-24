32a912c417ded672f3684f138f1d29b110feb77f
Alex Konrad <alexkonrad08@gmail.com>
2015-04-24 11:22:52 -0400

    Replace HTML Entities with tags

--- src/javascript/handlebars.js
+++ src/javascript/handlebars.js
@@ -1,9 +1,30 @@
 var handlebars = require('hbsfy/runtime');
+var $ = require('jquery')
 
 module.exports = (function() {
   return {
     registerHelpers: function() {
       handlebars.registerHelper('json', function(context) { return JSON.stringify(context) });
+      handlebars.registerHelper('escapeHTML', function(encoded) { 
+        var unencoded = $('<textarea />').html(encoded).val();
+        var inputText = unencoded.replace(/(?:\r\n|\r|\n)/g, '<br />');
+
+        var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+        //URLs starting with http://, https://, or ftp://
+        replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+        replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+        //Change email addresses to mailto:: links.
+        replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+        replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+        return replacedText;
+      })
     },
 
     registerPartials: function() {

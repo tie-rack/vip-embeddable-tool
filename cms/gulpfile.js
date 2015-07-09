67465c01a1159f1b53412692219c2fae06805e86
mickeysanchez <sanchez.mickey@gmail.com>
2015-07-09 12:49:29 -0400

    fix aws staging gulpfile push

--- cms/gulpfile.js
+++ cms/gulpfile.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 
 awsStaging = JSON.parse(fs.readFileSync('../aws-staging.json'));
 gulp.task("push-staging", function () {
-  gulp.src('/')
+  return gulp.src('**')
     .pipe(s3(awsStaging));
 })
 

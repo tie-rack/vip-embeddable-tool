4bc6978ce92cdbafd16489448828bb457a90c19d
Alex Konrad <alexkonrad08@gmail.com>
2016-05-31 18:21:26 -0400

    allow submitting different address for mail-in only states

--- src/javascript/views/view.js
+++ src/javascript/views/view.js
@@ -192,6 +192,7 @@ module.exports = (function() {
       this.find('#address-not-found h1').text(this.address);
       this.find('#address-input').value = "";
       this.hasSubmitted = false;
+      this.hasShownMailOnlyModal = false;
     },
 
     handleElectionData: function(response) {

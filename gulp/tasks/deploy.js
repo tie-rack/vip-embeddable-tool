bda52c7ab0fe9cb73cad61b82e21df62e2d3a287
Alex Konrad <alexkonrad08@gmail.com>
2016-09-13 17:52:59 -0700

    Add S3 deploy task for VIP tool

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,9 +1,37 @@
-// var gulp = require('gulp');
-// var s3 = require("gulp-s3");
-// var fs = require("fs");
+var gulp = require('gulp');
+var s3 = require('gulp-s3');
+var fs = require('fs');
 
-// awsStaging = JSON.parse(fs.readFileSync('aws-staging.json'));
-// gulp.task("push-staging", function () {
-//   gulp.src('./**')
-//     .pipe(s3(awsStaging));
-// })
+awsStaging = JSON.parse(fs.readFileSync('./aws-staging.json'));
+gulp.task("push-build-staging", function() {
+  return gulp.src('build/**')
+    .pipe(s3(awsStaging, {
+      uploadPath: '/'
+    }));
+});
+
+gulp.task('push-languages-staging', function() {
+  return gulp.src('src/javascript/languages/*')
+    .pipe(s3(awsStaging, {
+      uploadPath: '/languages/'
+    }));
+});
+
+gulp.task('deploy-staging', ['push-build-staging', 'push-languages-staging']);
+
+awsProduction = JSON.parse(fs.readFileSync('./aws-production.json'));
+gulp.task("push-build-production", function() {
+  return gulp.src('build/**')
+    .pipe(s3(awsProduction, {
+      uploadPath: '/'
+    }));
+});
+
+gulp.task('push-languages-production', function() {
+  return gulp.src('src/javascript/languages/*')
+    .pipe(s3(awsProduction, {
+      uploadPath: '/languages/'
+    }));
+});
+
+gulp.task('deploy-production', ['push-build-production', 'push-languages-production']);
